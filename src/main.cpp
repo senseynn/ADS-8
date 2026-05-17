d620f74d2b5e64e501606b11c8bd9a84c26e08c7
senseynn <163774648+senseynn@users.noreply.github.com>
2026-05-17 14:39:32 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 // Copyright 2021 NNTU-CS
+#include <string>
+
 #include "bst.h"
 
+void makeTree(BST<std::string>&, const char*);
+void printFreq(BST<std::string>&);
+
 int main() {
-    return 0;
+      BST<std::string> dic;
+  makeTree(dic, "src/war_peace.txt");
+  printFreq(dic);
+  return 0;
 }
